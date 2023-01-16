473344ea5521c568fc5efc20ad3db7f0ebfab143
KiaraAhmadnejad <92082964+KiaraAhmadnejad@users.noreply.github.com>
2023-01-16 11:02:16 +0100

    jobbat

---
+++ src/Filer.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Filer {
+    //skapar ett scanner objekt som hanterar input
+    Scanner myScanner = new Scanner();
+boolean addToFiler = false;
+boolean overwriteFiler = false;
+
+public Filer(){
+
+}
+
+}
